dfd6fc4c271b66ffb502a5857e25246423f87f09
catychelpan <catychelpan@gmail.com>
2025-11-18 23:33:59 +0200

    Fixing small FE bugs

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-1 text-gray-600'>
-            <li>+1-212-456-7890</li>
+            <li>+371 30943291</li>
             <li>Contact@readmore.com</li>
           </ul>
         </div>
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className=' font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className=' text-gray-500'>54709 Willms Station <br /> Graudu iela 68, Riga, Latvija</p>
+          <p className=' text-gray-500'>Graudu iela 68, Riga, Latvia</p>
           <p className=' text-gray-500'>Tel: +371 30943291 <br /> Email: support@readmore.com</p>
           <p className=' font-semibold text-xl text-gray-600'>Careers at Readmore</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
