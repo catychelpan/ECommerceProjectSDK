d4c65ac48e8caf48b87cb792b66fc8cdb65bce59
catychelpan <110096019+catychelpan@users.noreply.github.com>
2025-12-01 23:25:29 +0200

    fixing admin table layout styling

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -53,7 +53,7 @@ const List = ({ token }) => {
 
         {/* ------- List Table Title ---------- */}
 
-        <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
+        <div className='hidden md:grid grid-cols-[1fr_2fr_2fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
           <b>Image</b>
           <b>Name</b>
           <b>Author</b>
@@ -66,7 +66,9 @@ const List = ({ token }) => {
 
         {
           list.map((item, index) => (
-            <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
+            <div className='grid grid-cols-[1fr_3fr_1fr] 
+                md:grid-cols-[1fr_2fr_2fr_1fr_1fr_1fr] 
+                items-center gap-2 py-1 px-2 border text-sm' key={index}>
               <img className='w-12' src={item.image[0]} alt="" />
               <p>{item.name}</p>
               <p>{item.author}</p>
